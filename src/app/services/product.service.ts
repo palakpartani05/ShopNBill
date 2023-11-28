56dcbb08905810ea2e02dd5ab1b29042ea22c5c3
Palak Partani <139770730+palakpartani05@users.noreply.github.com>
2023-11-28 19:45:03 +0530

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -30,39 +30,3 @@ interface GetResponse {
   };
 }
 
-// // Import kar rahe hain kuch zaruri modules from Angular
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs'; // Observable ek component hai rxjs ka, jo asynchronous tasks jaise ki HTTP requests, user input handling, aur state changes ke liye common hai.
-// import { Product } from '../common/product';
-// import { map } from 'rxjs/operators'; // rxjs ka hissa hai - (Reactive Extensions for JavaScript), ye commonly asynchronous tasks ko handle karne mein use hota hai jaise ki HTTP requests, user input handling, aur state changes ko manage karna.
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProductService {
-//   private baseUrl = 'http://localhost:8080/api/products';
-//   // HttpClient module ka ek instance (httpClient) inject kiya gaya hai constructor mein.
-
-//   constructor(private httpClient: HttpClient) {
-
-//   }
-
-//   // getProductList() ek observable return karta hai jo product array ke sath connect hota hai.
-//   getProductList(): Observable<Product[]> {
-//    HttpClient ka get method use kiya gaya hai to get data from the specified URL (this.baseUrl).
-//     Fir, rxjs ka pipe method use kiya gaya hai data ko manipulate karne ke liye.
-//      Yahaan, map operator use kiya gaya hai jise data ko modify karke observable mein bhej sakte hain.
-//     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-//       map(response => response._embedded.products)
-//       map ek operator hai jo observable se aaye data ko manipulate karne mein help karta hai.
-//     );
-//   }
-// }
-
-// Spring Data Rest ke _embedded entry se JSON ko extract karne ke liye ek interface banaya gaya hai.
-// interface GetResponse {
-//   _embedded: {
-//     products: Product[];
-//   }
-// }
